0a4b22755d556b2f0393edd8d42a72b807afbeb8
Filip Chalupa <chalupa.filip@gmail.com>
2021-10-02 01:29:24 +0200

    Emulators

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,7 @@
 import * as functions from 'firebase-functions'
 
-console.log(functions) // @TODO: remove
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+	console.log(request)
+	functions.logger.info('Hello logs!', { structuredData: true })
+	response.send('Hello from Firebase!')
+})
