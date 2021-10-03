b97cec7f483a81654fb047721a9448769e911891
Filip Chalupa <chalupa.filip@gmail.com>
2021-10-03 21:44:52 +0200

    Body test

--- functions/create-invoice.js
+++ functions/create-invoice.js
@@ -1,5 +1,6 @@
-exports.handler = (event, context, callback) => {
-	callback(null, {
+exports.handler = async (event, context) => {
+	return {
 		statusCode: 200,
-	})
+		body: JSON.stringify({ message: 'Hello World' }),
+	}
 }
